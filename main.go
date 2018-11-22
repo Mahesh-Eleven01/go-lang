807c5cfde8101daa7e3d83a2764f6ef9422b7ed7
Mahesh-Eleven01 <mahesh@eleven01.io>
2018-11-22 16:17:44 +0530

    Added new function

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func add(){
+	fmt.Printf("This is an addition function\n");
+	a:= 64;
+	b:= 36;
+	c:= a+b;
+	fmt.Printf("Value after addition is %d \n",c);
+}
 func main(){
-	fmt.Printf("Hello Mahesh welcome to Go-lang !! ");
+	fmt.Printf("Hello Mahesh welcome to Go-lang !! \n");
+	add();
 }
